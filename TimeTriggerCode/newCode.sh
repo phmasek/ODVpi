e7928bbf4e5a5e37b36b29efe052f12dfa4a206b
Philip Masek <philip.masek@gmail.com>
2016-02-10 19:40:45 +0100

    Delete newCode.sh

--- TimeTriggerCode/newCode.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cat /media/sf_Thesis/MyTimeTrigger.cpp > MyTimeTrigger.cpp
-cat /media/sf_Thesis/MyTimeTrigger.hpp > MyTimeTrigger.hpp
-cat /media/sf_Thesis/configuration > configuration
-
-g++ -I /usr/include/opendavinci -c MyTimeTrigger.cpp -o MyTimeTrigger.o
-g++ -o timetrigger MyTimeTrigger.o -lopendavinci -lpthread -lrt
-
