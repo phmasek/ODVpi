0382d9b26bfc055706743b6b94d3af6eb9394ed5
Philip <philip.masek@gmail.com>
2016-02-10 13:37:17 -0500

    Added license and cleaned unnecessary files

--- TimeTriggerCode/MyTimeTrigger.hpp
+++ TimeTriggerCode/MyTimeTrigger.hpp
@@ -1,13 +1,14 @@
 /**
- * OpenDaVINCI - Tutorial.
- * Copyright (C) 2015 Christian Berger
+ * ODVpi - Benchmark code extending OpenDaVinci 
+ * (https://github.com/se-research/OpenDaVINCI)
+ * Copyright (C) 2016 Philip Masek, Magnus Thulin
  *
- * This library is free software; you can redistribute it and/or
+ * This code is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
+ * This code is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
